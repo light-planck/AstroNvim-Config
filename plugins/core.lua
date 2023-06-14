8a4c001ea667fad904fc97bee6a3b6e8eaf07af2
light <55218925+light-planck@users.noreply.github.com>
2023-06-14 10:24:23 +0900

    .

--- plugins/core.lua
+++ plugins/core.lua
@@ -28,14 +28,14 @@ return {
   {
     "L3MON4D3/LuaSnip",
     config = function(plugin, opts)
-      require "plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      require "plugins.configs.luasnip" (plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom luasnip configuration such as filetype extend or custom snippets
       require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user/snippets" } }
       local luasnip = require "luasnip"
       luasnip.filetype_extend("javascript", { "javascriptreact" })
     end,
   },
-  
+
   -- {
   --   "windwp/nvim-autopairs",
   --   config = function(plugin, opts)
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,15 +1,17 @@
 return {
-  "TimUntersberger/neogit",
-  version = "*",   -- Use for stability; omit to use `main` branch for the latest features
-  event = "VeryLazy",
-  dependencies = {
-    "sindrets/diffview.nvim",
+  {
+    "TimUntersberger/neogit",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    dependencies = {
+      "sindrets/diffview.nvim",
+    },
+    config = function()
+      require("neogit").setup {
+        integrations = {
+          diffview = true,
+        },
+      }
+    end,
   },
-  config = function()
-    require("neogit").setup {
-      integrations = {
-        diffview = true,
-      },
-    }
-  end,
 }
